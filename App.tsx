70b5d619825608be75e1de42fb6d2748a2ce6eca
Suprema UX <70323230+supremaux@users.noreply.github.com>
2026-01-03 07:38:19 -0400

    versão corrigida

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
 
-export default function App() {
+export const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: "http://projetos.supremaux.com/aliancasgold/" }}
+      />
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
